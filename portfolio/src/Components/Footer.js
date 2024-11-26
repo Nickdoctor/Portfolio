64538101a1af2b1384c6f969b9fdf1818a281b08
Nickdoctor <ngugliemo@csus.edu>
2024-11-25 18:28:16 -0800

    Added to footer

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Box, Container, Typography, Link, IconButton } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Footer = () => {
   return (
-              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <IconButton
-                  aria-label="Instagram"
-                  component="a"
-                  href="https://www.instagram.com/nickdoctor2/"
-                  target = "_blank"
-                  size="large"
-                >
-                  <InstagramIcon />
-                </IconButton>
-              </Box>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <IconButton
+        aria-label="Instagram"
+        component="a"
+        href="https://www.instagram.com/nickdoctor2/"
+        target="_blank"
+        size="large"
+      >
+        <InstagramIcon />
+      </IconButton>
+      <IconButton
+        aria-label="GitHub"
+        component="a"
+        href="https://github.com/Nickdoctor"
+        target="_blank"
+        size="large"
+      >
+        <GitHubIcon />
+      </IconButton>
+      <IconButton
+        aria-label="LinkedIn"
+        component="a"
+        href="https://www.linkedin.com/in/nicolas-gugliemo-5776631b9/"
+        target="_blank"
+        size="large"
+      >
+        <LinkedInIcon />
+      </IconButton>
+    </Box>
   );
 };
 
