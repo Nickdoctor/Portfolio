ddbc48a74453b89f9af44cfefc2c1a858610110a
Nickdoctor <ngugliemo@csus.edu>
2024-11-28 00:33:23 -0800

    Added button styles

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -15,18 +15,22 @@ const MyNavbar = () => {
 
     return (
         <Navbar expand= "lg" className="bg-body-tertiary d-flex justify-content-center align-items-center">
-            <Container className = "mx-auto">
-            <Navbar.Brand onClick={() => handleNavigation('/')}>
+            <Container className = "justify-content-center">
+            <Navbar.Brand onClick={() => handleNavigation('/')} className='justify-content-center text-center'>
                 My Portfolio
             </Navbar.Brand>
             <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
             <Navbar.Collapse id = "basic-navbar-nav">
-                <Nav className = "mx-auto">
-                <Nav.Link onClick={() => handleNavigation('/')}>Home</Nav.Link>
-                <Nav.Link onClick={() => handleNavigation('/AboutMe')}>About Me</Nav.Link>
-                <Nav.Link onClick={() => handleNavigation('/Portfolio')}>Portfolio</Nav.Link>
-                <Nav.Link onClick={() => handleNavigation('/ContactMe')}>Contact Me</Nav.Link>
+                <Nav className = "justify-content-center d-flex justify-content-center gap-5">
+                <Nav.Link  className="btn btn-primary me-2" onClick={() => handleNavigation('/')}>Home</Nav.Link>
+                <Nav.Link  className="btn btn-primary me-2" onClick={() => handleNavigation('/AboutMe')}>About Me</Nav.Link>
+                <Nav.Link  className="btn btn-primary me-2" onClick={() => handleNavigation('/Portfolio')}>Portfolio</Nav.Link>
+                <Nav.Link  className="btn btn-primary me-2" onClick={() => handleNavigation('/ContactMe')}>Contact Me</Nav.Link>
             </Nav>
+            <Nav className="ms-auto gap-3">
+        <Nav.Link className="btn btn-primary me-2" onClick={() => handleNavigation('/Login')}>Login</Nav.Link>
+        <Nav.Link className="btn btn-outline-secondary" onClick={() => handleNavigation('/Register')}>Signup</Nav.Link>
+             </Nav>
         </Navbar.Collapse>
     </Container>
 </Navbar>
